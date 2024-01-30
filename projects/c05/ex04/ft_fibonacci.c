d9aa7fa0ef93ab8529fb130b54f3d8f0852eaec8
Joao Vitor Ribeiro Souza <joao-vri@c1r7s2.42porto.com>
2024-01-30 11:10:40 +0000

    c05

--- projects/c05/ex04/ft_fibonacci.c
+++ projects/c05/ex04/ft_fibonacci.c
@@ -6,23 +6,27 @@
 /*   By: joao-vri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 18:12:59 by joao-vri          #+#    #+#             */
-/*   Updated: 2024/01/29 18:13:04 by joao-vri         ###   ########.fr       */
+/*   Updated: 2024/01/30 11:08:21 by joao-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_fibonacci(int index)
 {
+	if (index < 0)
+		return (-1);
 	if (index == 0)
 		return (0);
 	if (index == 1)
 		return (1);
 	if (index >= 0)
 		return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+	else
+		return (0);
 }
 
 /*#include <stdio.h>
 
 int	main(void)
 {
-	printf("%i\n", ft_fibonacci(10));
+	printf("%i\n", ft_fibonacci(-10));
 }*/
